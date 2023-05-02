8eb601da024e3e8a8a55a1dd939bb7be36a92b3c
veeraagam <veer_aagam@gmail.com>
2023-05-02 14:59:08 +0530

    first commit

---
+++ LiftMaster/PJSIPSDK/PJSIPSDK/PJSIPSDK.h
@@ -0,0 +1,18 @@
+//
+//  PJSIPSDK.h
+//  PJSIPSDK
+//
+//  Created by vTechnolabs on 28/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PJSIPSDK.
+FOUNDATION_EXPORT double PJSIPSDKVersionNumber;
+
+//! Project version string for PJSIPSDK.
+FOUNDATION_EXPORT const unsigned char PJSIPSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PJSIPSDK/PublicHeader.h>
+
+
---
+++ LiftMaster/PhoneSipManagerSDK/PhoneSipManagerSDK/PhoneSipManagerSDK.h
@@ -0,0 +1,18 @@
+//
+//  PhoneSipManagerSDK.h
+//  PhoneSipManagerSDK
+//
+//  Created by vTechnolabs on 28/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhoneSipManagerSDK.
+FOUNDATION_EXPORT double PhoneSipManagerSDKVersionNumber;
+
+//! Project version string for PhoneSipManagerSDK.
+FOUNDATION_EXPORT const unsigned char PhoneSipManagerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhoneSipManagerSDK/PublicHeader.h>
+
+
